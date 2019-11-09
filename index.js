29e6802a793d77892afd275d5543498b622c2e67
Deogracias Rey Bonifacio <deogracias.rey.bonifacio@gmail.com>
2019-11-09 09:33:18 +0800

    added navigation and edited its color to white

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 new fullpage('#main', {
   autoScrolling: true,
+  navigation: true,
+  navigationTooltips: ['Home', 'About Me', 'Education', 'Work Experience', 'Skill', 'Hobbies', 'Contact Me'],
   scrollingSpeed: 1500,
 });
