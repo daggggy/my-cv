2d6d00568a9c8c7b1cf27cc19c9547fea5fb199c
Deogracias Rey Bonifacio <deogracias.rey.bonifacio@gmail.com>
2019-11-09 20:21:24 +0800

    added force scroll top on refresh javascript

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ new fullpage('#main', {
   navigationTooltips: ['Home', 'About Me', 'Education', 'Work Experience', 'Skill', 'Hobbies', 'Contact Me'],
   scrollingSpeed: 1500,
 });
+
+$(window).on('beforeunload', function() {
+  $(window).scrollTop(0);
+});
+
+$(document).ready(function() {
+  $(this).scrollTop(0);
+});
